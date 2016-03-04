4746d3d12d0e5b8ab9c59ef2a7172685df83c750
LesPaul21 <aupe5089@colorado.edu>
2016-03-04 15:49:38 -0700

    Simple rotating code to rotate platforms

---
+++ Rotator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+    public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector2(0, 15) * Time.deltaTime * speed);
+	}
+}
