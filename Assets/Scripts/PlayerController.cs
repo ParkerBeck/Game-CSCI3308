fa0327c53370b3ed27b49d73afa031fedb34e232
S95F <stfu2360@colorado.edu>
2016-03-09 13:15:27 -0700

    added Death and Animations
    Now should restart loaded level upon death, we can change that to instead load a death level, and also added some animations. I don't know if they will be download right. I added some code to the movement so it flips the entire character so the animations would be easier and also triggers the animations.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,25 +6,55 @@ public class PlayerController : MonoBehaviour
 
     public float speed;
     public float jumpPower;
+	bool facingRight = true;
+	Animator anim; 
 
     private Rigidbody2D rb2d;
     // Use this for initialization
     void Start()
     {
         rb2d = gameObject.GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-
         float moveHorziontal = Input.GetAxis("Horizontal");
-        //float moveVertical = Input.GetAxis("Vertical");
         rb2d.AddForce((Vector2.right * speed) * moveHorziontal);
+
+		if (moveHorziontal == 0) {
+			anim.SetBool ("Movin", false);
+		}
+
+		if (moveHorziontal != 0) {
+			anim.SetBool ("Movin", true);
+		}
+
+		//anim.SetFloat("Speed", Mathf.Abs(moveHorziontal));
+
+		// this code is so we can face different directions
+		if (moveHorziontal < 0 && facingRight) {
+			flip ();
+		}
+		else if (moveHorziontal > 0 && !facingRight) 
+		{
+			flip ();
+		}
+
 		if (Input.GetKeyDown (KeyCode.UpArrow)) {
 			if (rb2d.velocity.y <= .2 && rb2d.velocity.y >= -.2) {
 				rb2d.AddForce ((Vector2.up * jumpPower));
 			}
 		}
     }
+
+	void flip()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
 }
---
+++ Assets/Scripts/death.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class death : MonoBehaviour {
+	//private Rigidbody2D rb2d;
+	// Use this for initialization
+	private Scene scene;
+	void Start () {
+		scene = SceneManager.GetActiveScene();
+		//rb2d = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 playerPos = transform.position;
+		if(playerPos.y < -2.5) {
+			//rb2d.AddForce (Vector2.up * 100);
+
+			SceneManager.LoadScene(scene.name);
+			//Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+}
