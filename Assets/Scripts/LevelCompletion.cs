3501ac406033c54a09ce7da239d9962e9bf8e79c
LesPaul21 <aupe5089@colorado.edu>
2016-03-24 10:16:03 -0600

    Level Completion Script
    Should be added to a Collider with "Is Trigger" checked.

---
+++ Assets/Scripts/LevelCompletion.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelCompletion : MonoBehaviour {
+
+    public string LevelName;
+    public GUIText endLevelText;
+    bool endLevel;
+
+    void Update ()
+    {
+        if(endLevel && Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene(LevelName);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            endLevelText.gameObject.SetActive(true);
+            endLevel = true;
+        }
+    }
+}
