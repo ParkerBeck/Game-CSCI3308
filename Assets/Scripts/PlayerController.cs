9807c5b029036bef65f5c5f5e845c79395f3368f
ParkerBeck <pbeck27@gmail.com>
2016-04-04 16:37:19 -0600

    Use raytrace for player jump code and enable multi jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,26 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections; 
 
 public class PlayerController : MonoBehaviour
 {
 
     public float speed;
     public float jumpPower;
+	public int jumpCount;
+
+	int jumpsLeft;
 	bool facingRight = true;
 	Animator anim; 
 
     private Rigidbody2D rb2d;
+	private PolygonCollider2D collider;
+
     // Use this for initialization
     void Start()
     {
         rb2d = gameObject.GetComponent<Rigidbody2D>();
+		//transform = gameObject.GetComponent<Transform> ();
+		collider = gameObject.GetComponent<PolygonCollider2D>();
 		anim = GetComponent<Animator>();
     }
 
@@ -22,14 +29,8 @@ public class PlayerController : MonoBehaviour
     {
         float moveHorziontal = Input.GetAxis("Horizontal");
         rb2d.AddForce((Vector2.right * speed) * moveHorziontal);
+		anim.SetBool ("Movin", moveHorziontal != 0);
 
-		if (moveHorziontal == 0) {
-			anim.SetBool ("Movin", false);
-		}
-
-		if (moveHorziontal != 0) {
-			anim.SetBool ("Movin", true);
-		}
 
 		//anim.SetFloat("Speed", Mathf.Abs(moveHorziontal));
 
@@ -42,11 +43,17 @@ public class PlayerController : MonoBehaviour
 			flip ();
 		}
 
-		if (Input.GetKeyDown (KeyCode.UpArrow)) {
-			if (rb2d.velocity.y <= .2 && rb2d.velocity.y >= -.2) {
-				rb2d.AddForce ((Vector2.up * jumpPower));
-			}
+		if (onGround ()) {
+			jumpsLeft = jumpCount;
 		}
+
+		//Should single jump if up is held down, double jump only while up is triggered
+		if (jumpsLeft == jumpCount && Input.GetKey (KeyCode.UpArrow)) {
+			jump ();
+		} else if (jumpsLeft > 0 && Input.GetKeyDown (KeyCode.UpArrow)) {
+			jump ();
+		}
+	
     }
 
 	void flip()
@@ -56,5 +63,21 @@ public class PlayerController : MonoBehaviour
 		theScale.x *= -1;
 		transform.localScale = theScale;
 	}
+
+	void jump()
+	{
+		rb2d.AddForce ((Vector2.up * jumpPower));
+		jumpsLeft--;
+	}
+
+	bool onGround()
+	{
+		//Terain is on 9th layer
+		int terrainMask = 1 << 9;
+		//Returns true if the player has terrain under
+		return Physics2D.Raycast(transform.position, Vector2.down, collider.bounds.extents.y + .05f, terrainMask);
+	}
+
+
 
 }
