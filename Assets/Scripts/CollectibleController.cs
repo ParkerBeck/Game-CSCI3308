5402447bb28decddcc6f3b5283b5cf325b95dcdb
Helper2002 <jake.broadwater@gmail.com>
2016-04-04 22:03:51 -0600

    half finished commit
    Hey, maybe if the fourth 3308 assignment had directions that actually worked, then maybe I would have had the time to finish this script!

---
+++ Assets/Scripts/CollectibleController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectibleController : MonoBehaviour {
+
+	private int wtr;
+	private bool collided;
+	Animator anim;
+	private BoxCollider2D boxCollider;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		boxCollider = GetComponent<BoxCollider2D> (); //not a rigidbody2d to avoid gravitational effects
+		wtr = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		int playermask = 1 << 4; //set to whatever layer player is on
+		bool up = Physics2D.Raycast(transform.position, Vector2.up, collider.bounds.extents.y + .05f, playerMask);
+		bool down = Physics2D.Raycast(transform.position, Vector2.down, collider.bounds.extents.y + .05f, playerMask);
+		bool left = Physics2D.Raycast(transform.position, Vector2.left, collider.bounds.extents.y + .05f, playerMask);
+		bool right = Physics2D.Raycast(transform.position, Vector2.right, collider.bounds.extents.y + .05f, playerMask);
+		if (up || down || left || right) /// if player collides with collectible
+		{
+			collided = true;
+		}
+		if (collided) //waits 30 frames before expiring so an animation can play?
+		{
+			wtr++;
+			//if (wtr == 30)
+				//killself
+		}
+	}
+}
