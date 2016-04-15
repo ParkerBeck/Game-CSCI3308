2af280bc2ea5146f6239c1d5ca99eb30b9bf0024
ParkerBeck <pbeck27@gmail.com>
2016-04-15 14:41:01 -0600

    Fix PlayerController again and change terrain prefabs to the terrain layer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,16 +6,22 @@ public class PlayerController : MonoBehaviour
 
     public float speed;
     public float jumpPower;
-	bool facingRight = true;
-	Animator anim; 
-	private GameObject Camera;
+    public int jumpCount;
+
+    int jumpsLeft;
+    bool facingRight = true;
+    Animator anim;
+
     private Rigidbody2D rb2d;
+    private PolygonCollider2D collider;
+
     // Use this for initialization
     void Start()
     {
         rb2d = gameObject.GetComponent<Rigidbody2D>();
-		anim = GetComponent<Animator>();
-		Camera = GameObject.FindGameObjectWithTag ("MainCamera");
+        //transform = gameObject.GetComponent<Transform> ();
+        collider = gameObject.GetComponent<PolygonCollider2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -23,42 +29,54 @@ public class PlayerController : MonoBehaviour
     {
         float moveHorziontal = Input.GetAxis("Horizontal");
         rb2d.AddForce((Vector2.right * speed) * moveHorziontal);
+        anim.SetBool("Movin", moveHorziontal != 0);
 
-		if (moveHorziontal == 0) {
-			anim.SetBool ("Movin", false);
-		}
+        // this code is so we can face different directions
+        if (moveHorziontal < 0 && facingRight)
+        {
+            flip();
+        }
+        else if (moveHorziontal > 0 && !facingRight)
+        {
+            flip();
+        }
 
-		if (moveHorziontal != 0 || rb2d.velocity.y > 0) {
-			anim.SetBool ("Movin", true);
-		}
+        if (onGround())
+        {
+            jumpsLeft = jumpCount;
+        }
 
-		//anim.SetFloat("Speed", Mathf.Abs(moveHorziontal));
+        //Should single jump if up is held down, double jump only while up is triggered
+        if (jumpsLeft == jumpCount && Input.GetKey(KeyCode.UpArrow))
+        {
+            jump();
+        }
+        else if (jumpsLeft > 0 && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            jump();
+        }
 
-		// this code is so we can face different directions
-		if (moveHorziontal < 0 && facingRight) {
-			flip ();
-		}
-		else if (moveHorziontal > 0 && !facingRight) 
-		{
-			flip ();
-		}
-		if(transform.position.x < (Camera.transform.position.x-5)){
-			transform.position = new Vector3 (Camera.transform.position.x-5, transform.position.y, transform.position.z);
-			rb2d.velocity = new Vector2 (0, rb2d.velocity.y);
-		}
-		if (Input.GetKeyDown (KeyCode.UpArrow)) {
-			if (rb2d.velocity.y <= .2 && rb2d.velocity.y >= -.2) {
-				rb2d.AddForce ((Vector2.up * jumpPower));
-			}
-		}
     }
 
-	void flip()
-	{
-		facingRight = !facingRight;
-		Vector3 theScale = transform.localScale;
-		theScale.x *= -1;
-		transform.localScale = theScale;
-	}
+    void flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
 
+    void jump()
+    {
+        rb2d.AddForce((Vector2.up * jumpPower));
+        jumpsLeft--;
+    }
+
+    bool onGround()
+    {
+        //Terain is on 9th layer
+        int terrainMask = 1 << 9;
+        //Returns true if the player has terrain under
+        return Physics2D.Raycast(transform.position, Vector2.down, collider.bounds.extents.y + .05f, terrainMask);
+    }
 }
