418da4cdd0674e9db10677bbd8ed8432c93bc263
LesPaul21 <aupe5089@colorado.edu>
2016-04-18 08:52:40 -0600

    Added functionality to the rotator script

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -1,9 +1,13 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Rotator : MonoBehaviour {
 
     public float speed;
+    public int yRotate;
+    public int xRotate;
+    public int yMove;
+    public int xMove;
 	// Use this for initialization
 	void Start () {
 	
@@ -11,6 +15,7 @@ public class Rotator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(new Vector2(0, 15) * Time.deltaTime * speed);
+        transform.Rotate(new Vector2(xRotate, yRotate) * Time.deltaTime * speed);
+        transform.Translate(new Vector2(xMove, yMove) * Time.deltaTime * speed);
 	}
 }
